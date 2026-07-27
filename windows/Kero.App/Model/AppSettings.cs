c170280a84109ef23da9bb4953ad3e5dbee169a2
Rayzerrek <rayserrek@gmail.com>
2026-07-27 23:14:33 +0200

    Fix dark text, overlay alt-tab, closing tabs behavior, and fetch installed fonts

--- windows/Kero.App/Model/AppSettings.cs
+++ windows/Kero.App/Model/AppSettings.cs
@@ -19,10 +19,10 @@ public class AppSettings : ObservableObject
         set { if (Set(ref _themeName, value)) Save(); }
     }
 
-    public string[] AvailableFonts { get; } = new[] 
-    { 
-        "Cascadia Mono", "Cascadia Code", "Consolas", "Courier New", "Fira Code", "JetBrains Mono", "Hack", "Source Code Pro", "Ubuntu Mono", "MesloLGS NF" 
-    };
+    public string[] AvailableFonts { get; } = System.Windows.Media.Fonts.SystemFontFamilies
+        .Select(f => f.Source)
+        .OrderBy(f => f)
+        .ToArray();
 
     private string _fontFamily = "Cascadia Mono";
     public string FontFamily
