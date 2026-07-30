b544ca93cb7b32078cc748a74f3d75a3b1170582
Rayzerrek <rayserrek@gmail.com>
2026-07-30 18:14:02 +0200

    perf: optimize text rendering with GlyphRun and enable Server GC

--- Vendor/XtermSharp/CharData.cs
+++ Vendor/XtermSharp/CharData.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Diagnostics;
 
-namespace XtermSharp {
+namespace XtermSharp
+{
 	// MIGUEL TODO:
 	// The original code used Rune + Code, but it really makes no sense to keep those separate, excpt for null that has a
 	// zero-width thing for code 0.
 	[DebuggerDisplay("[CharData (Attr={Attribute},Rune={Rune},W={Width},Code={Code})]")]
-	public struct CharData {
+	public struct CharData
+	{
 		public int Attribute;
 		public Rune Rune;
 		public int Width;
@@ -17,17 +19,17 @@ namespace XtermSharp {
 		public const int DefaultAttr = Renderer.DefaultColor << 9 | (256 << 0);
 		public const int InvertedAttr = Renderer.InvertedDefaultColor << 9 | (256 << 0) | Renderer.InvertedDefaultColor;
 
-		public static CharData Null = new CharData (DefaultAttr, '\u0200', 1, 0);
-		public static CharData WhiteSpace = new CharData (DefaultAttr, ' ', 1, 32);
-		public static CharData LeftBrace = new CharData (DefaultAttr, '{', 1, 123);
-		public static CharData RightBrace = new CharData (DefaultAttr, '}', 1, 125);
-		public static CharData LeftBracket = new CharData (DefaultAttr, '[', 1, 91);
-		public static CharData RightBracket = new CharData (DefaultAttr, ']', 1, 93);
-		public static CharData LeftParenthesis = new CharData (DefaultAttr, '(', 1, 40);
-		public static CharData RightParenthesis = new CharData (DefaultAttr, ')', 1, 41);
-		public static CharData Period = new CharData (DefaultAttr, '.', 1, 46);
+		public static CharData Null = new CharData(DefaultAttr, '\u0200', 1, 0);
+		public static CharData WhiteSpace = new CharData(DefaultAttr, ' ', 1, 32);
+		public static CharData LeftBrace = new CharData(DefaultAttr, '{', 1, 123);
+		public static CharData RightBrace = new CharData(DefaultAttr, '}', 1, 125);
+		public static CharData LeftBracket = new CharData(DefaultAttr, '[', 1, 91);
+		public static CharData RightBracket = new CharData(DefaultAttr, ']', 1, 93);
+		public static CharData LeftParenthesis = new CharData(DefaultAttr, '(', 1, 40);
+		public static CharData RightParenthesis = new CharData(DefaultAttr, ')', 1, 41);
+		public static CharData Period = new CharData(DefaultAttr, '.', 1, 46);
 
-		public CharData (int attribute, Rune rune, int width, int code)
+		public CharData(int attribute, Rune rune, int width, int code)
 		{
 			Attribute = attribute;
 			Rune = rune;
@@ -36,7 +38,7 @@ namespace XtermSharp {
 		}
 
 		// Returns an empty CharData with the specified attribute
-		public CharData (int attribute)
+		public CharData(int attribute)
 		{
 			Attribute = attribute;
 			Rune = '\u0200';
@@ -55,7 +57,7 @@ namespace XtermSharp {
 		/// <summary>
 		/// Returns true if this CharData matches the given Rune, irrespective of character attributes
 		/// </summary>
-		public bool MatchesRune (CharData chr)
+		public bool MatchesRune(CharData chr)
 		{
 			return Rune == chr.Rune;
 		}
