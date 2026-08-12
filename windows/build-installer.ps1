f600f2a46fd2d0a3a481f795515704804adaeee9
Rayzerrek <rayserrek@gmail.com>
2026-08-12 15:38:21 +0200

    chore(build): fix installer desktop copy path for current user

--- windows/build-installer.ps1
+++ windows/build-installer.ps1
@@ -38,7 +38,7 @@ if ($LASTEXITCODE -ne 0) {
 
 # 4. Copy generated MSI to publish folder and Desktop
 $MsiSource = Join-Path $ScriptDir "Vex.Setup\bin\Release\VexSetup.msi"
-$DesktopTarget = "C:\Users\kacpe\Desktop\Vex.msi"
+$DesktopTarget = "C:\Users\Ziut\Desktop\Vex.msi"
 
 if (Test-Path $MsiSource) {
     $MsiTarget = Join-Path $InstallerOutputDir "Vex.msi"
